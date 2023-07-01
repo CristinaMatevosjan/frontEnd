d29b2a1073fa8bf29c4bda8d46813a5c26b41128
Kristina M <c.listeva@yandex.ru>
2023-07-01 14:50:10 +0300

    add scss, page product

---
+++ product.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    // loop: true,
+    // autoplay:true,
+    // slidesPerView: 2,
+    centeredSlides: true,
+      
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+});
